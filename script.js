00064d58ed79b02e5eb25113ce87a067f6e49b86
Amanda Skinner <amandamuscha@yahoo.com>
2023-02-02 20:45:49 -0500

    Game ends when it runs out of questions

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 class GameMaker {
-    constructor(question, opt1, opt2, opt3, opt4, scr, tot, scoreCount, questionCount) {
-        this.question = question;
-        this.opt1 = opt1;
-        this.opt2 = opt2;
-        this.opt3 = opt3;
-        this.opt4 = opt4;
-        this.scr = scr;
-        this.tot = tot;
-        this.scoreCount = scoreCount;
-        this.questionCount = questionCount;
-        this.changeText();
-    }
-    changeText() {
-        this.question.innerText = qVal
-        this.opt1.innerText = o1Val
-        this.opt2.innerText = o2Val
-        this.opt3.innerText = o3Val
-        this.opt4.innerText = o4Val
-        this.scoreCount = scoreCount
-        this.questionCount = questionCount
-        this.scr.innerText = "Score: " + scoreCount.toString()
-        this.tot.innerText = "Questions: " + questionCount.toString()
-    }
-    //https://www.wavsource.com/sfx/sfx.htm - Credit for Sounds
+  constructor(question, opt1, opt2, opt3, opt4, scr, tot, scoreCount, questionCount) {
+    this.question = question;
+    this.opt1 = opt1;
+    this.opt2 = opt2;
+    this.opt3 = opt3;
+    this.opt4 = opt4;
+    this.scr = scr;
+    this.tot = tot;
+    this.scoreCount = scoreCount;
+    this.questionCount = questionCount;
+    this.changeText();
+  }
+  changeText() {
+    this.question.innerText = qVal
+    this.opt1.innerText = o1Val
+    this.opt2.innerText = o2Val
+    this.opt3.innerText = o3Val
+    this.opt4.innerText = o4Val
+    this.scoreCount = scoreCount
+    this.questionCount = questionCount
+    this.scr.innerText = "Score: " + scoreCount.toString()
+    this.tot.innerText = "Questions: " + questionCount.toString()
+  }
+  //https://www.wavsource.com/sfx/sfx.htm - Credit for Sounds
 }
 const question = document.getElementById('data-question')
 const opt1 = document.getElementById('opt1')
@@ -39,7 +39,6 @@ const btn4 = document.getElementById('btn4')
 wrongSound = new Audio('explosion_x.wav')
 rightSound = new Audio('chime.wav')
 
-
 var scoreCount = 0
 var questionCount = 0
 var qVal = "Question 1: Ex"
@@ -49,75 +48,109 @@ var o3Val = "Option3"
 var o4Val = "Option4"
 var ans = 0
 var guess = 0
+var qindex = 0;
 
 const game = new GameMaker(question, opt1, opt2, opt3, opt4, scr, tot);
-var questTest = "This Question came from the text file|TFO1|TFO2|TFO3|TFO4|1";
-assignQuestion(questTest);
+var questTest = ["This Question came from the text file|TFO1|TFO2|TFO3|TFO4|1","Tees Question came from the text file|TFO1|TFO2|TFO3|TFO4|1"];
+
+function shuffleQuestions(array) {
+  var i = array.length,
+      j = 0,
+      temp;
+
+  while (i--) {
+
+      j = Math.floor(Math.random() * (i+1));
+
+      // swap randomly chosen element with current element
+      temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+
+  }
+
+  return array;
+}
+
+shuffleQuestions(questTest);
+
+
+assignQuestion(questTest[0]);
 
 function assignQuestion(qTest) {
-    const qArr = qTest.split("|");
-    qVal = qArr[0];
-    o1Val = qArr[1];
-    o2Val = qArr[2];
-    o3Val = qArr[3];
-    o4Val = qArr[4];
-    ans = qArr[5];
-    game.changeText();
+  const qArr = qTest.split("|");
+  qVal = qArr[0];
+  o1Val = qArr[1];
+  o2Val = qArr[2];
+  o3Val = qArr[3];
+  o4Val = qArr[4];
+  ans = qArr[5];
+  game.changeText();
 }
 
 function wrongAns() {
   wrongSound.play()
   questionCount++
-  qVal="Incorrect!"
-  setTimeout(function() {
-    assignQuestion("This|1|2|3|4|1")
-}, (1000));
+  qindex++;
+  qVal = "Incorrect!"
+  setTimeout(function () {
+    assignQuestion(questTest[qindex])
+  }, (1000));
+  if(qindex>=questTest.length){
+    qVal = "Thanks for Playing!"
+  }
 }
 
-function rightAns(){
+function rightAns() {
   rightSound.play()
   scoreCount++
   questionCount++
-  qVal="Correct!"
-  setTimeout(function() {
-    assignQuestion("This|1|2|3|4|1")
-}, (1000));
+  qindex++;
+  qVal = "Correct!"
+  setTimeout(function () {
+    assignQuestion(questTest[qindex])
+  }, (1000));
+  if(qindex>=questTest.length){
+    qVal = "Thanks for Playing!"
+  }
 }
 
-btn1.addEventListener("click", function() {
-    guess=1;
-    if(guess==ans){
-      rightAns();
-    }else{
-      wrongAns();
-    }
-    game.changeText();
-  });
+btn1.addEventListener("click", function () {
+  guess = 1;
+  if (guess == ans) {
+    rightAns();
+  } else {
+    wrongAns();
+  }
+  game.changeText();
+});
 
-btn2.addEventListener("click", function() {
-    guess=2;
-    if(guess==ans){
-      rightAns();
-    }else{
-      wrongAns();
-    }
-    game.changeText();
-  });
-  btn3.addEventListener("click", function() {
-    guess=3;
-    if(guess==ans){
-      rightAns();
-    }else{
-      wrongAns();
-    }
-    game.changeText();
-  });
-  btn4.addEventListener("click", function() {
-    guess=4;
-    if(guess==ans){
-      rightAns();
-    }else{
-      wrongAns();
-    }
-    game.changeText();
-  });
+btn2.addEventListener("click", function () {
+  guess = 2;
+  if (guess == ans) {
+    rightAns();
+  } else {
+    wrongAns();
+  }
+  game.changeText();
+});
+btn3.addEventListener("click", function () {
+  guess = 3;
+  if (guess == ans) {
+    rightAns();
+  } else {
+    wrongAns();
+  }
+  game.changeText();
+});
+
+btn4.addEventListener("click", function () {
+  guess = 4;
+  if (guess == ans) {
+    rightAns();
+  } else {
+    wrongAns();
+  }
+  game.changeText();
+});
+
